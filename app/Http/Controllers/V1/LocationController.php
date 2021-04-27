b2183c6bb670681da26b4e76fa3b131706bb7bdf
Pharoouzy <pharoouzy@yahoo.com>
2021-04-27 13:34:25 +0100

    Create Location endpoints

--- app/Http/Controllers/V1/LocationController.php
+++ app/Http/Controllers/V1/LocationController.php
@@ -11,11 +11,36 @@ class LocationController extends Controller {
 
     use RequestHelper;
 
+    public function index() {
+        $locations = Location::orderBy('id')->get();
+        return $this->response('Locations successfully retrieved', $locations);
+    }
+
     public function search(Request $request) {
         return $this->response('Success', []);
     }
 
     public function store(Request $request) {
+        try{
+            $location = Location::create([
+                'address' => $request->address,
+                'street_name' => $request->main,
+                'country' => $request->sec,
+                'lat' => $request->lat,
+                'lng' => $request->lng,
+            ]);
+            return $this->response('Location successfully saved', $location);
+        } catch(\Exception $exception){
+            return $this->response('An error occured', $exception->getMessage(), 422);
+        }
+    }
 
+    public function show($id) {
+        try{
+            $location = Location::findOrFail($id);
+            return $this->response('Location successfully deleted');
+        } catch(\Exception $exception){
+            return $this->response('An error occured', $exception->getMessage(), 422);
+        }
     }
 }
